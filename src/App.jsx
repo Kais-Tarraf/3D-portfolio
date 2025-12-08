91cb021093fb3460f64229e321ad04f36bd4e6c9
Kais Tarraf <khtkht661100@gmail.com>
2025-12-08 14:41:24 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+const App = () => {
+	return (
+		<main>
+			<div>
+				<h1 className="text-amber-300 underline">Welcome to my 3d portfolio</h1>
+			</div>
+		</main>
+	);
+};
+
+export default App;
