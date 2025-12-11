e7e2d58dc8ee8b1121dac9117e6d436ef404b9da
Kais Tarraf <hossamtr184@gmail.com>
2025-12-11 04:16:13 +0300

    implement logos section

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import Navbar from "./components/Navbar";
 import Hero from "./components/sections/Hero";
 import Showcase from "./components/sections/Showcase";
-
+import LogoSection from "./components/LogoSection";
 const App = () => {
 	return (
 		<>
 			<Navbar />
 			<Hero />
 			<Showcase />
+			<LogoSection />
 		</>
 	);
 };
